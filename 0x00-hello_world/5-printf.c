097167a73de87ea33bf526ff7ab3fdffffa1becf
Bowofoluwa <bowofoluwaolawunmi@gmail.com>
2023-10-18 10:08:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - A program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line sing printf function
  * Return: 0
@@ -6,6 +7,6 @@
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
